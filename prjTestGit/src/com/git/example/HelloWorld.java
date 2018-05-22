997da38ef1762c807d08fb13f38217ecc940d938
jsrk31 <39477914+jsrk31@users.noreply.github.com>
2018-05-22 14:24:31 +0530

    Update HelloWorld.java

--- prjTestGit/src/com/git/example/HelloWorld.java
+++ prjTestGit/src/com/git/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.git.example;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World - Test Git...edited in git");
+		System.out.println("Hello World - Test Git...edited in git..");
 	}
 }
